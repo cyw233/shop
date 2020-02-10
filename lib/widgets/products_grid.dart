9c2a48278f823665a6757f6be1c5f2bc97c382a9
Colin Wang <chenyangw233@gmail.com>
2020-02-10 17:20:25 +1100

    Refactoring code

--- lib/widgets/products_grid.dart
+++ lib/widgets/products_grid.dart
@@ -18,10 +18,9 @@ class ProductsGrid extends StatelessWidget {
         crossAxisSpacing: 10,
         mainAxisSpacing: 10,
       ),
-      itemBuilder: (ctx, i) => ProductItem(
-        products[i].id,
-        products[i].title,
-        products[i].imageUrl,
+      itemBuilder: (ctx, i) => ChangeNotifierProvider.value(
+        value: products[i],
+        child: ProductItem(),
       ),
     );
   }
