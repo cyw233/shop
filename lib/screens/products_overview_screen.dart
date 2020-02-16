929fbb86272593b1150621e2afaebe9c2d11a7f1
Colin Wang <chenyangw233@gmail.com>
2020-02-16 11:32:02 +1100

    Add order item widget (211)

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -6,6 +6,7 @@ import '../widgets/products_grid.dart';
 import '../widgets/badge.dart';
 import '../providers/cart.dart';
 import './cart_screen.dart';
+import '../widgets/app_drawer.dart';
 
 enum FilterOptions {
   Favorites,
@@ -62,6 +63,7 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
           ),
         ],
       ),
+      drawer: AppDrawer(),
       body: ProductsGrid(_showOnlyFavorites),
     );
     return scaffold;
